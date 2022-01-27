a6a7e2e9aa4f455e57f3b7ecb08306f54c0c1fcb
Vaibhav Vemula <vaibhav25vemula23@gmail.com>
2022-01-27 17:51:16 +0530

    dr

--- src/Components/TeamCard.js
+++ src/Components/TeamCard.js
@@ -4,18 +4,18 @@ const TeamCard = (props) => {
     // const dr = 'https://drive.google.com/uc?export=view&id='+props.data.img
   return (
     <>
-      <div data-aos="fade-in" data-aos-delay="50" className="md:w-72 mx-auto bg-white rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
+      <div data-aos="fade-in" data-aos-delay="50" className="md:w-72 mx-auto  rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
         <div>
           <img
-            className="object-center object-fill h-72 w-72"
+            className="object-center object-fill md:h-72 md:w-72 h-40 w-40 rounded-full md:rounded-full"
             src={props.data.img}
             alt="v"
             referrerpolicy="no-referrer"
           ></img>
         </div>
-        <div className="text-center py-8 sm:py-6">
-          <p className="text-2xl text-gray-700 font-bold mb-2">{props.data.name}</p>
-          <p className="text-lg text-gray-500 font-normal">
+        <div className="text-center py-3 md:py-6">
+          <p className="text-xl md:text-2xl text-gray-700 font-bold mb-1">{props.data.name}</p>
+          <p className="text-base md:text-lg text-gray-500 font-normal">
             {props.data.role}
           </p>
         </div>
--- src/images/TeamDetails.js
+++ src/images/TeamDetails.js
@@ -14,6 +14,7 @@ import sak from './teamPhotos/Shreyas A Katti.jpg'
 import sb from './teamPhotos/Shreesh Bajpai.jpg'
 import akb from './teamPhotos/Akshay K B.jpg'
 import dr from './teamPhotos/Divyansh Raina.jpg'
+
 export const TeamDetails = [
     {
         name:'Muskan Kothari',
@@ -51,7 +52,7 @@ export const TeamDetails = [
         role:'Events Team'
     },
     {
-        name:'Sriram Radhakrishna',
+        name:'Sriram R',
         email:'sriram.radhakrishna42@gmail.com',
         linedin:'https://www.linkedin.com/in/sriram-radhakrishna-665885207/',
         img:sr,
@@ -93,7 +94,7 @@ export const TeamDetails = [
         role:'Operations Team'
     },
     {
-        name:'Shauryadeepsinh G Raolji',
+        name:'Shauryadeepsinh',
         email:'raoljishauryadeep@gmail.com',
         linedin:'https://www.linkedin.com/in/shauryadeepsinh-g-raolji-299225203',
         img:sgr,
