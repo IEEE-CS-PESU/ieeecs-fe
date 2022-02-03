f721d4b290e8af91929edf9c166e8866b940bd7b
Vaibhav Vemula <vaibhav25vemula23@gmail.com>
2022-02-03 15:01:56 +0530

    events page

--- src/Events.js
+++ src/Events.js
@@ -1,88 +1,74 @@
-import React, { useState, useEffect, useRef } from "react";
-import NET from "vanta/dist/vanta.waves.min";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInstagram, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { NavLink } from "react-router-dom";
+import React, { Suspense, useEffect } from "react";
+import Model from "./models/Model";
+
+const images = [
+  // Front
+  {
+    position: [0, 0, 1.5],
+    rotation: [0, 0, 0],
+    tex: 0,
+    url: "Inauguration",
+    but: "Register Now",
+  },
+  // Back
+  {
+    position: [-0.9, 0, -0.6],
+    rotation: [0, 0, 0],
+    tex: 1,
+    url: "PreviousEvents",
+    but: "Explore",
+  },
+  {
+    position: [0.9, 0, -0.6],
+    rotation: [0, 0, 0],
+    tex: 2,
+    url: "UpcomingEvents",
+    but: "Explore",
+  },
+  // Left
+  {
+    position: [-1.65, 0, 0.25],
+    rotation: [0, Math.PI / 3, 0],
+    tex: 3,
+    url: "Community",
+    but: "Join",
+  },
+  {
+    position: [-2.15, 0, 1.5],
+    rotation: [0, Math.PI / 2.5, 0],
+    tex: 4,
+    url: "Spotlight",
+    but: "Check Out",
+  },
+
+  // Right
+  {
+    position: [1.65, 0, 0.25],
+    rotation: [0, -Math.PI / 3, 0],
+    tex: 5,
+    url: "Recruitment",
+    but: "Join Us",
+  },
+  {
+    position: [2.15, 0, 1.5],
+    rotation: [0, -Math.PI / 2.5, 0],
+    tex: 6,
+    url: "Projects",
+    but: "Explore",
+  },
+];
 
 const Events = () => {
-  const [vantaEffect, setVantaEffect] = useState(0);
-  const myRef = useRef(null);
   useEffect(() => {
-    window.scrollTo(0,0)
-    if (!vantaEffect) {
-      setVantaEffect(
-        NET({
-          el: myRef.current,
-          mouseControls: true,
-          touchControls: true,
-          gyroControls: true,
-          minHeight: 200.0,
-          minWidth: 200.0,
-          scale: 1.0,
-          scaleMobile: 1.0,
-          color: 0x050505,
-          shininess: 60.0,
-          waveHeight: 17.5,
-          zoom: 0.65,
-        })
-      );
-    }
-    return () => {
-      if (vantaEffect) vantaEffect.destroy();
-    };
-  }, [vantaEffect]);
+    // window.location ='/events' 
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
-      <div ref={myRef} className="bg-black h-auto pt-24 text-white text-4xl">
-        <div className="bg-home px-8 w-full">
-          <div
-            className="
-                    py-12
-                    flex flex-col
-                    justify-between
-                    min-h-screen
-                    md:max-w-2xl
-                    lg:max-w-3xl
-                    mx-auto
-                "
-          >
-            <div>
-              <div className="mt-12 text-white text-center">
-                <h1 className="font-bold text-5xl md:text-7xl">
-                  Events Coming Soon
-                </h1>
-                <p className="pt-10 text-lg md:text-xl">We are almost there!</p>
-                <p className="pt-2 text-lg md:text-xl">
-                  We are getting ready to launch our first event. Meanwhile
-                  follow us on social media or contact us via email.
-                </p>
-              </div>
-              <div className="flex justify-center mt-9">
-                <a href="#!" type="button" className="pr-10">
-                  <FontAwesomeIcon icon={faInstagram} className="text-4xl" />
-                </a>
-                <a
-                  href="https://www.linkedin.com/company/ieee-cs-pesu/"
-                  type="button"
-                  className="pr-10"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FontAwesomeIcon icon={faLinkedinIn} className="text-4xl" />
-                </a>
-                <a href="mailto:ieee.cs.rr@pes.edu" type="button" className="">
-                  <FontAwesomeIcon icon={faEnvelope} className="text-4xl" />
-                </a>
-              </div>
-              <div className="w-full text-center pt-12">
-          <NavLink to="/contact" className="text-xl bg-[#f8a219] hover:bg-[#ffb53f] text-white font-bold py-5 px-10 rounded">
-            Contact Us
-          </NavLink>
-        </div>
-            </div>
-          </div>
-        </div>
+      <div className="bg-black text-4xl w-full h-full pt-20">
+        <Suspense fallback={<h1 className="text-white">loading</h1>}>
+          <Model images={images} />
+        </Suspense>
       </div>
     </>
   );
---
+++ src/models/Model.js
@@ -0,0 +1,189 @@
+import * as THREE from "three";
+import { useEffect, useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useCursor, Image, Text } from "@react-three/drei";
+import { useRoute, useLocation } from "wouter";
+import { NavLink } from "react-router-dom";
+
+const GOLDENRATIO = 1.61803398875;
+
+const dat = {
+  Inauguration: ["Register Now!", "urrrrrr", true],
+  PreviousEvents: ["Explore", "/", false],
+  UpcomingEvents: ["Explore", "/", false],
+  Community: ["Join", "https://forms.gle/7vo3ktrDBhfKaGZS7", true],
+  Spotlight: ["Check Out!", "/", false],
+  Recruitment: ["Join Us!", "https://forms.gle/GbDbCKpQqp4cbGG58", true],
+  Projects: ["Explore", "/", false],
+  false: ["Explore", "/", false],
+};
+
+export default function Model({ images }) {
+  const [vis, setVis] = useState(false);
+  const [nnn, setnnn] = useState("false");
+  return (
+    <>
+      <Canvas gl={{ alpha: false }} dpr={[1, 1.5]} camera={{ fov: 70 }}>
+        <color attach="background" args={["#202023"]} />
+        {/* <fog attach="fog" args={["#202023", 0, 15]} /> */}
+        <group position={[0, -0.5, 1.2]}>
+          <Frames images={images} fun={setVis} na={setnnn} />
+        </group>
+      </Canvas>
+      {vis && (
+        <div className="relative z-10 top-[-200px] w-full pt-10 text-center">
+          {dat[nnn][2] ? (
+            <a
+              href={dat[nnn][1]}
+              target="_blank"
+              rel="noreferrer"
+              type="button"
+              className="px-5 py-2 bg-slate-100 rounded-3xl text-xl shadow-inner hover:bg-[#f8a219]"
+            >
+              {dat[nnn][0]}
+            </a>
+          ) : (
+            <NavLink
+              to={dat[nnn][1]}
+              className="px-5 py-2 bg-slate-100 rounded-3xl text-xl shadow-inner hover:bg-[#f8a219]"
+            >
+              {dat[nnn][0]}
+            </NavLink>
+          )}
+        </div>
+      )}
+    </>
+  );
+}
+
+function Frames({
+  images,
+  fun,
+  na,
+  q = new THREE.Quaternion(),
+  p = new THREE.Vector3(),
+}) {
+  const ref = useRef();
+  const clicked = useRef();
+  const [, params] = useRoute("/events/:id");
+  const [, setLocation] = useLocation();
+
+  useEffect(() => {
+    clicked.current = ref.current.getObjectByName(params?.id);
+    if (clicked.current) {
+      clicked.current.parent.updateWorldMatrix(true, true);
+      clicked.current.parent.localToWorld(p.set(0, GOLDENRATIO / 2, 1.25));
+      clicked.current.parent.getWorldQuaternion(q);
+    } else {
+      p.set(0, 0, 5.5);
+      q.identity();
+    }
+  });
+  useFrame((state, dt) => {
+    state.camera.position.lerp(p, THREE.MathUtils.damp(0, 1, 3, dt));
+    state.camera.quaternion.slerp(q, THREE.MathUtils.damp(0, 1, 3, dt));
+  });
+  return (
+    <>
+      <group
+        ref={ref}
+        onClick={(e) => {
+          e.stopPropagation();
+          console.log(e.object);
+          clicked.current === e.object ? fun(false) : fun(true);
+          clicked.current === e.object ? na("false") : na(e.object.name);
+          setLocation(
+            clicked.current === e.object
+              ? "/events"
+              : "/events/" + e.object.name
+          );
+        }}
+        onPointerMissed={() => {
+          setLocation("/events");
+          fun(false);
+        }}
+      >
+        {images.map((props) => (
+          <Frame key={props.url} {...props} />
+        ))}
+      </group>
+    </>
+  );
+}
+
+function Frame({ url, tex, c = new THREE.Color(), ...props }) {
+  const [hovered, hover] = useState(false);
+  //   const [rnd] = useState(() => Math.random());
+  const image = useRef();
+  const frame = useRef();
+  //   const name = getUuid(url);
+  const name = url;
+  useCursor(hovered);
+  useFrame((state) => {
+    // image.current.material.zoom =
+    //   2 + Math.sin(rnd * 10000 + state.clock.elapsedTime / 3) / 2;
+    image.current.scale.x = THREE.MathUtils.lerp(
+      image.current.scale.x,
+      0.85 * (hovered ? 1 : 1),
+      0.1
+    );
+    image.current.scale.y = THREE.MathUtils.lerp(
+      image.current.scale.y,
+      0.9 * (hovered ? 1 : 1),
+      0.1
+    );
+    frame.current.material.color.lerp(
+      c.set(hovered ? "orange" : "white").convertSRGBToLinear(),
+      0.1
+    );
+  });
+  return (
+    <group {...props}>
+      <mesh
+        name={name}
+        onPointerOver={(e) => {
+          e.stopPropagation();
+          hover(true);
+        }}
+        onPointerOut={() => hover(false)}
+        scale={[1, GOLDENRATIO, 0.05]}
+        position={[0, GOLDENRATIO / 2, 0]}
+      >
+        <boxGeometry />
+        <meshStandardMaterial
+          color="#151515"
+          metalness={0.5}
+          roughness={0.5}
+          envMapIntensity={2}
+        />
+        <mesh
+          ref={frame}
+          raycast={() => null}
+          scale={[0.9, 0.93, 0.9]}
+          position={[0, 0, 0.2]}
+        >
+          <boxGeometry />
+          <meshBasicMaterial toneMapped={false} fog={false} />
+        </mesh>
+        <Image
+          raycast={() => null}
+          ref={image}
+          position={[0, 0, 0.7]}
+          url="https://images.unsplash.com/photo-1599420186946-7b6fb4e297f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3687&q=80"
+        />
+        {/* <Html position={[0, 0, 0]} transform>
+            <button style={{height:'1px'}}>hfh</button>
+        </Html> */}
+      </mesh>
+      <Text
+        maxWidth={0.1}
+        anchorX="left"
+        anchorY="top"
+        position={[0.55, GOLDENRATIO, 0]}
+        fontSize={0.025}
+      >
+        {name}
+      </Text>
+    </group>
+  );
+}
