6b9b8c88c6e3ea4b7ed01cf3c7350b2d90350d61
Hita Juneja <hitajuneja@gmail.com>
2022-06-24 01:22:59 +0530

    IG spotlight

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./Components/NavBar";
 import Home from "./Home";
 import Events from "./Events";
+import InstaFeed from "./Components/InstaFeed";
 import AOS from "aos";
 import "aos/dist/aos.css";
 AOS.init();
@@ -18,6 +19,7 @@ function App() {
           <Route path="/contact" element={<Home comp={"contact"} />} />
           <Route path="/team" element={<Home comp={"team"} />} />
           <Route path="/events" element={<Events />} />
+          <Route path="/spotlight" element={<InstaFeed token={process.env.REACT_APP_ACCESS_TOKEN} limit={30}/>}/>
         </Routes>
         <Footer />
       </BrowserRouter>
---
+++ src/Components/InstaFeed.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+import React, { useEffect, useState, useRef } from "react";
+import InstaPost from "./InstaPost";
+
+
+
+export default function Feed({ token, ...props }) {
+  // const REACT_VERSION = React.version;
+  // console.log(REACT_VERSION)
+  const [feeds, setFeedsData] = useState([]);
+  //use useRef to store the latest value of the prop without firing the effect
+  const tokenProp = useRef(token);
+  tokenProp.current = token;
+  const onInit = () => {
+    console.log("lightGallery has been initialized");
+  };
+
+  useEffect(() => {
+    // this is to avoid memory leaks
+    const abortController = new AbortController();
+    // console.log(token)
+    let url = `https://graph.instagram.com/me/media?fields=id,username,media_type,media_url,timestamp,permalink,caption&limit=${props.limit}&access_token=${token}`;
+    async function fetchInstagramPost() {
+      try {
+        axios
+          .get(url)
+          .then((resp) => {
+            setFeedsData(resp.data.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      } catch (err) {
+        console.log("error", err);
+      }
+    }
+
+    // manually call the fetch function
+    fetchInstagramPost();
+
+    return () => {
+      // cancel pending fetch request on component unmount
+      abortController.abort();
+    };
+  }, [props.limit]);
+
+  return (
+    <div id="container" className="grid lg:grid-cols-3 md:grid-cols-2">
+        {feeds.map((feed,i) => (
+            i<3?
+                // <div className="border border-red-400 my-20 pr-12 mr-8 py-5" key={i}>
+                <div className="my-20 pr-12 mr-8 py-5" key={i}>
+                    <InstaPost key={feed.id} feed={feed} />
+                </div>
+            :
+
+            // <div className="border border-emerald-400 my-2 pr-12 mr-8 py-5" key={i}>
+            <div className="my-2 pr-12 mr-8 py-5" key={i}>
+                <InstaPost key={feed.id} feed={feed} />
+            </div>
+          
+        ))}
+    </div>
+  );
+}
