e5445789b08a670b517996e325653142efe3818f
Veeresh R G <veeresh.ravi@gmail.com>
2022-07-07 16:50:01 +0530

    :rocket: Teams Page

--- src/Components/Team.js
+++ src/Components/Team.js
@@ -27,7 +27,7 @@ const Team = React.forwardRef((props, ref) => {
             </p>
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {TeamData &&
               TeamData.map((iii, i) => {
                 return <TeamCard data={iii} key={i} />;
