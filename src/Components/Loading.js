a03863e92560406ec0c05cbd71f19d4c2aa41b51
Hita Juneja <hitajuneja@gmail.com>
2022-07-10 00:34:35 +0530

    Added loading component to spotlight

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -11,11 +11,10 @@ const load_option = {
 };
 
 const Loading = () => {
-    console.log("loaded")
+    console.log("loaded here")
     return (
         <div className="loading flex items-center justify-center h-20 w-20" style={{ opacity: 0.8 }}>
             {/* <h1 className="text-3xl text-red-800">Loading..</h1> */}
-            
             <Lottie options={load_option} height={150} width={150} />
         </div>
     );
