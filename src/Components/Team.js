9873d55702de4580495d41aba7debb974a924ab6
Veeresh R G <veeresh.ravi@gmail.com>
2022-07-22 21:34:50 +0530

    :zap: Team UI Update :rocket:

--- src/Components/Team.js
+++ src/Components/Team.js
@@ -2,37 +2,142 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import TeamCard from "./TeamCard";
 import EventsBanner from "./EventsBanner";
+import Loading from "./Loading";
 
 const Team = React.forwardRef((props, ref) => {
-  const [TeamData, setTeam] = useState([]);
-  const url = 'https://ieeecspesu.herokuapp.com'
+
+
+  //Defining the state to Store the result of the below Axios GET request
+  const [, setTeam] = useState([]);
+
+
+  //Hooks to show current / past members
+
+  const [present, setPresent] = useState(true);
+  const [currData, setCurrData] = useState([]);
+  const [xData, setXData] = useState([]);
+
+
+  //Axios Get request
+  const url = "https://ieeecspesu.herokuapp.com";
   useEffect(() => {
-    axios.get(`${url}/coreteam`).then((res) => {
-      // console.log(res.data)
-      setTeam(res.data);
-      // console.log(res.data);
-    });
+    axios.get(`${url}/coreteam`)
+      .then((res) => {
+
+        //Ex-Members Array
+        const Xnames = ["Monika", "Pramod Seshasayanan P",
+          "Konkala Manisha", "K S Ramalakshmi"];
+
+        const currentMembers = (obj) => {
+
+          return Xnames.includes(obj.name) === false;
+        }
+
+
+
+        const checkXMembers = (obj) => {
+
+          return Xnames.includes(obj.name);
+        }
+
+        //Sorting Function according to id field
+        const sortedObj = (res.data).sort((a, b) => {
+          return Number(a.id) - Number(b.id);
+        })
+
+        setTeam(sortedObj);
+
+        setCurrData(res.data.filter(currentMembers));
+        setXData(res.data.filter(checkXMembers))
+      })
+      .catch(err => {
+        console.log("Error")
+      })
+
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  //For Loading Symbol
+  const loading = [<div></div>, <Loading />, <div></div>];
+
+  //Testing
+  /* 
+  Team.forEach((item) => {
+    console.log((item.id))
+  })
+  */
+
+
+  const styles =
+  {
+    active:
+    {
+      backgroundColor: "#FACC17",
+      color: "white",
+      fontWeight: "700",
+
+    },
+
+    inActive:
+    {
+
+    }
+  }
+
+  // top-[-115px] md:top-[-150px]
   return (
     <>
       <div ref={ref} className="h-auto bg-slate-100">
-        <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 pb-12 md:pb-24 pt-0">
+        <section className="max-w-6xl text-center mx-auto px-4 sm:px-6 lg:px-4 pb-12 md:pb-24">
           <div data-aos="fade-up" className="flex flex-col items-center">
             <span className="text-gray-200">
-              <div className="md:text-[150px] text-[125px]">100001</div>
+              <div className="md:text-[150px] text-[105px] px-3">011100</div>
             </span>
-            <p className="text-[#000000] font-sans md:text-7xl text-5xl relative top-[-115px] md:top-[-150px]">
+            <p className="text-[#000000] font-sans md:text-7xl text-5xl absolute top-16 md:top-20 ">
               Our Team
             </p>
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {TeamData &&
-              TeamData.map((iii, i) => {
-                return <TeamCard data={iii} key={i} />;
-              })}
+          <div className="mb-10 flex justify-center">
+            <button style={present ? styles.active : styles.inActive} className="p-2 text-[1.2rem] rounded-tl-xl rounded-bl-xl border-2 border-yellow-400" onClick={(prev) => {
+              setPresent(true)
+
+            }}>Current Members
+            </button>
+
+            <button style={!present ? styles.active : styles.inActive} className="p-2 text-[1.2rem] rounded-tr-xl rounded-br-xl border-2 border-yellow-400 " onClick={(prev) => {
+              setPresent(false)
+
+            }}>Ex Members
+            </button>
+
           </div>
+
+          <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto">
+            {
+              (currData.length > 0 || xData.length > 0) ?
+
+                (present === true ? currData : xData).map((item, ind) => { return <TeamCard data={item} key={ind + 1} /> }
+                ) :
+
+                loading.map((item, ind) => {
+                  return (
+                    ind % 2 === 0 ? <div /> : <div className="text-center mx-auto"><Loading /></div>
+                  )
+                }
+
+
+                )}
+
+
+
+          </div>
+
+          {/* <a href="/exmembers" rel="noopener noreferrer" target="_blank" type="button" className="text-white mt-10 bg-[#F8A219] hover:bg-[#F8A219]/80 focus:ring-4 focus:outline-none focus:ring-[#FF9119]/50 font-medium rounded-lg text-xl animate-bounce px-5 py-4 text-center inline-flex items-center dark:hover:bg-[#FF9119]/80 dark:focus:ring-[#FF9119]/40 mr-2 mb-2">
+            <FaUniversalAccess className="mr-5" />
+            View Past Members
+          </a> */}
         </section>
       </div>
       <EventsBanner />
@@ -41,3 +146,16 @@ const Team = React.forwardRef((props, ref) => {
 });
 
 export default Team;
+
+//  {/* finalData && finalData.length === 0
+//                 ? loading.map((iii, i) => {
+//                   return (
+//                     <div className="mx-auto">
+//                       {i % 2 === 0 ? <div></div> : <Loading />}
+//                     </div>
+//                   );
+//                 })
+//                 : finalData.map((iii, i) => {
+//                   return <TeamCard data={iii} key={i} />;
+//                 }) */}
+
--- src/Components/TeamCard.js
+++ src/Components/TeamCard.js
@@ -2,30 +2,30 @@ import React from "react";
 import { FaLinkedin } from "react-icons/fa";
 // import { Link, NavLink } from "react-router-dom";
 const TeamCard = (props) => {
-  // console.log(props.data)
   return (
     <>
       <div
         data-aos="fade-in"
         data-aos-delay="50"
-        className="md:w-72 mx-20 md:mx-auto hover:scale-105 transition duration-300 rounded-lg shadow-lg overflow-hidden flex flex-col justify-center items-center"
+        className="md:w-72 mx-auto  rounded-lg md:shadow-lg overflow-hidden md:overflow-hidden flex flex-col justify-center items-center"
       >
-        <div className="w-full">
+        <div>
           <img
-            className="object-center  w-full object-fill md:h-72 md:w-72 h-84  rounded-md "
+            className="object-center object-fill md:h-72 md:w-72 h-40 w-40 rounded-full md:rounded-none"
             src={props.data.image}
             alt="v"
             referrerPolicy="no-referrer"
           ></img>
         </div>
-        <div className="text-center py-3 md:py-6 w-full">
+        <div className="text-center py-3 md:py-6">
           <p className="text-xl md:text-2xl text-gray-700 font-bold mb-1">
             {props.data.name}
           </p>
           <p className="text-base md:text-lg text-gray-500 font-normal">
             {props.data.role}
           </p>
-          <p className=" mx-12 py-1 md:mx-28 flex items-center  justify-around  mt-5  rounded-2xl">
+
+          <p className=" mx-12 md:mx-28 flex items-center  justify-around mt-1  rounded-2xl">
             {/* <p className="sm:tracking-[0px]  text-sm tracking-[1.3px]  md:text-lg lg:text-xl uppercase py-1 text-blue-700 font-bold">Profile</p> */}
             <a target="_blank" href={props.data.linkedin} rel="noreferrer" > <FaLinkedin className=" text-blue-700  h-5 w-5 md:h-8 md:w-8" /></a>
 
